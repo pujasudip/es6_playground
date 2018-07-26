54f25040dcb2a25816e8013dc69c736909c27ea6
Sudip Baral <pujasudip@gmail.com>
2018-07-26 15:52:07 -0700

    completed class demo

--- app/index.js
+++ app/index.js
@@ -1,1 +1,11 @@
-console.log('App working: open app/index.js to start exploring ES6');
+import {Person, SuperPerson} from './person';
+
+const bob = new Person('Bob');
+
+console.log(bob.speak('My name is Samjhana!'));
+
+const superSarah = new SuperPerson('Sarah', 'Tip toe real quite');
+
+superSarah.speak('Hi I\'am super Sarah.');
+
+superSarah.saySuperpower();
